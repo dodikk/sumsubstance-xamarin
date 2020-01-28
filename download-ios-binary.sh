2bedc8a7ece2c219f1aeb1413e0cd68dffdf4799
dodikk <dodikk88.reg@gmail.com>
2020-01-28 14:19:26 +0200

    [ci] added download script for framework binaries

---
+++ download-ios-binary.sh
@@ -0,0 +1,20 @@
+
+LAUNCH_DIR=$PWD
+
+cd ..
+    REPOSITORY_ROOT_DIR=$PWD
+cd "$LAUNCH_DIR"
+
+
+
+cd "$REPOSITORY_ROOT_DIR/lib-native-ios"
+curl \
+  https://raw.githubusercontent.com/SumSubstance/KYC-iOS-Release/master/2.11.1/SumSubstanceKYC.zip \
+  > SumSubstanceKYC.zip
+
+unzip SumSubstanceKYC.zip \
+      -d SumSubstanceKYC-frameworks
+cd "$LAUNCH_DIR"
+
+
+cd "$LAUNCH_DIR"
