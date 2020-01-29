9d80d2793da62478c675c18d231baa1559666fd4
dodikk <dodikk88.reg@gmail.com>
2020-01-29 13:47:29 +0200

    [ios] added wrapped native code from vendor's demo

--- app/SumSubDemo-ios/ViewController.cs
+++ app/SumSubDemo-ios/ViewController.cs
@@ -1,4 +1,5 @@
 ﻿using Foundation;
+using SumSubstance.iOS;
 using System;
 using UIKit;
 
@@ -10,11 +11,72 @@ namespace SumSubDemo_ios
         {
         }
 
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            StartKyc();
+
+        }
+
+        private void StartKyc()
+        {
+            SSEngine kycEngine = SumSubSdkHelper.GetInstance();
+
+            // https://developers.sumsub.com/msdk/ios.html#usage
+            // Then you should:
+
+            // Connect to remote - [engine connectWithExpirationHandler:verificationResultHandler:]
+            // -
+            kycEngine.ConnectWithExpirationHandler(
+                tokenExpirationHandler: () =>
+                {
+                    // TODO: better error handling in production app
+                    // -
+                    Console.WriteLine("[connect] Token Expired");
+
+                    // refresh example :
+                    // https://github.com/SumSubstance/KYC-iOS-Demo/blob/master/Demo/Logic/Coordinator.m#L242
+                },
+                verificationResultHandler: (isVerificationOk) =>
+                {
+                    // [alex-d] it's ok to not proceed in this callback according to vendor's sample
+                    // seems like `ConnectWithExpirationHandler()` is performed in synchronous and blocking manner
+                    // so this might affect app performance
+                    // -
+
+                    Console.WriteLine($"[connect] verification done. IsVerified={isVerificationOk}");
+                });
+
+            kycEngine.OnActionEvent += (ssActionEventInfo) =>
+            {
+                Console.WriteLine($"[kyc] action received. Step={ssActionEventInfo.FlowStep}");
+            };
+
+
+            var formattedChatTitle = new NSAttributedString("SumSub KYC iOS demo");
+            UINavigationController chatViewController =
+                SSFacade.GetChatControllerWithAttributedTitle(title: formattedChatTitle);
+
+            this.NavigationController.PresentViewController(
+                viewControllerToPresent: chatViewController,
+                animated: true,
+                completionHandler: () =>
+                {
+                    Console.WriteLine("[kyc] [chat] Chat VC presented");
+                });
+
+            // TODO: dispose chat properly
+            // https://github.com/SumSubstance/KYC-iOS-Demo/blob/master/Demo/Logic/Coordinator.m#L271
+        }
+
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
         }
+
 
         public override void DidReceiveMemoryWarning()
         {
