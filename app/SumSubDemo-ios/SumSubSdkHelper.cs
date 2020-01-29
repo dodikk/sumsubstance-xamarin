826cfdb06e7d6c27708ac7e3693e4dd84abc8dfa
dodikk <dodikk88.reg@gmail.com>
2020-01-29 13:23:05 +0200

    [ios] added initialization helper class to native project

---
+++ app/SumSubDemo-ios/SumSubSdkHelper.cs
@@ -0,0 +1,39 @@
+﻿using Foundation;
+using SumSubstance.iOS;
+
+
+namespace SumSubDemo_ios
+{
+    public static class SumSubSdkHelper
+    {
+        public static SSEngine GetInstance()
+        {
+            // TODO: [alex-d] maybe improve thread safety
+            // by implementing double-check singleton pattern with mutex/lock
+            // -
+
+            if (_engineSingleton != null)
+            {
+                return _engineSingleton;
+            }
+
+            // https://developers.sumsub.com/msdk/ios.html#usage
+            // -
+            _engineSingleton = SSFacade.SetupForApplicant(
+                applicantID: "", // your applicant identifier
+                token: "", // your Sum&Sub auth token
+                locale: NSLocale.CurrentLocale.LocaleIdentifier.ToString(),
+                supportEmail: "",
+                baseUrl: "test-msdk.sumsub.com", // baseUrl - test-msdk.sumsub.com for test environment or msdk.sumsub.com for production one
+                colorConfig: null, // nil or subclass of KYCColorConfig (for color pallet customization)
+                imageConfig: null  // nil or subclass of KYCImageConfig (for icons customization)
+            );
+
+            return _engineSingleton;
+        }
+
+
+        private static SSEngine _engineSingleton;
+
+    }
+}
