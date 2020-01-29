9bbd8f62a64e5119df5199266f5c0be6ae902830
dodikk <dodikk88.reg@gmail.com>
2020-01-29 19:55:30 +0200

    [droid] switched to wget since curl downloaded inconsistent files

--- scripts/download-droid-binary.sh
+++ scripts/download-droid-binary.sh
@@ -7,11 +7,17 @@ cd "$LAUNCH_DIR"
 
 cd "$REPOSITORY_ROOT_DIR/lib-bindings/SumSubSDK/SumSubSDK.Core.droid/Jars"
 
-curl https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-core/3.0.1/:kyc-core-3.0.1.aar \
-     > kyc-core.aar
+#curl https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-core/3.0.1/:kyc-core-3.0.1.aar \
+#     > kyc-core.aar
+wget \
+    --output-document=kyc-core.aar \
+    https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-core/3.0.1/:kyc-core-3.0.1.aar
 
-curl https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-core/3.0.1/:kyc-core-3.0.1.pom \
-     > kyc-core.pom
+# curl https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-core/3.0.1/:kyc-core-3.0.1.pom \
+#      > kyc-core.pom
+wget \
+    --output-document=kyc-core.pom \
+    https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-core/3.0.1/:kyc-core-3.0.1.pom
 
 cd "$LAUNCH_DIR"
 
@@ -19,11 +25,16 @@ cd "$LAUNCH_DIR"
 
 cd "$REPOSITORY_ROOT_DIR/lib-bindings/SumSubSDK/SumSubSDK.Client.droid/Jars"
 
-curl https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-client/3.0.1/:kyc-client-3.0.1.aar \
-     > kyc-client.aar
+# curl https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-client/3.0.1/:kyc-client-3.0.1.aar \
+#      > kyc-client.aar
+wget \
+    --output-document=kyc-client.aar \
+    https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-client/3.0.1/:kyc-client-3.0.1.aar
 
-curl https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-client/3.0.1/:kyc-client-3.0.1.pom \
-     > kyc-client.pom
-
+# curl https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-client/3.0.1/:kyc-client-3.0.1.pom \
+#      > kyc-client.pom
+wget \
+    --output-document=kyc-client.pom \
+    https://dl.bintray.com/sumsub/maven/com/sumsub/kyc-client/3.0.1/:kyc-client-3.0.1.pom
 
 cd "$LAUNCH_DIR"
